848aefb0d8d7197443750a13d18989fbc89dfd6b
Christian Abueg <cja2400@gmail.com>
2020-05-07 14:56:49 -0700

    Can open folders and Sublime text thru g++

--- testFile.cpp
+++ testFile.cpp
@@ -1,26 +1,45 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <bits/stdc++.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
 using namespace std;
+void createFolder(string name);
 
 /*
 class NoteAutomator
 {
 private:
 public:
-};
-*/
+};*/
+
+
+void createFolder(string name)
+{
+    const char * nameOfFolder = name.c_str();
+    if (mkdir(nameOfFolder, 0777) == -1) 
+        cerr << "Error: " << strerror(errno) << endl; 
+  
+    else
+        cout << "Directory created"; 
+    return;
+}
 
 
 int main(int argc, char const *argv[])
 {
-    cout << argc << " arguments detected" << endl;
+    string noteName = argv[1];
+    string folderName = argv[2];
+    string ext = argv[3];
+
     
-    for (int i = 0; i < argc; i++)
-    {
-        cout << argv[i] << endl;
-    }
-    
+
+    string systemCommand = "subl ";
+    systemCommand += noteName;
+    system(systemCommand.c_str());
+
     return 0;
 }
 
